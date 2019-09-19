b6eef712b81c4a72e37d15cb63ea98f1a2109a49
boxboo <kazushi593641@gmail.com>
2019-09-19 12:06:54 +0900

    スクリプトを作成

---
+++ Assets/Kazuhi/KazushiScript/Kaiju.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kaiju : MonoBehaviour
+{
+    public RectTransform kaijuRectTransform;
+    [SerializeField]private float kaijuSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovesKaiju();
+    }
+
+    void MovesKaiju()
+    {
+        kaijuRectTransform.localPosition+=new Vector3(kaijuSpeed,0,0);
+    }
+}
